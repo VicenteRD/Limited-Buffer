28709aae95470334ca233db91d8d01c0f043bcc3
Vicente Rivera <vicente.drd@gmail.com>
2017-06-02 11:38:06 -0400

    Add debugging messages and finalize things

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ import sys
 from threading import Thread, Semaphore
 
 BUFFER_SIZE = 1000
-PRODUCER_COUNT = 3 * BUFFER_SIZE
+PRODUCER_COUNT = 1.5 * BUFFER_SIZE
 CONSUMER_COUNT = 1.5 * BUFFER_SIZE
+
+PROD_MIN = 2
+CONS_MIN = 2
+PROD_MAX = PROD_MIN + 8
+CONS_MAX = CONS_MIN + 8
+
+LOG_LEVEL = logging.DEBUG
 
 logger = logging.getLogger()
 
@@ -18,24 +25,43 @@ class LimitedBuffer:
         self._size = size
         self._used = 0
 
+        # Sempahore to check if producers can act
         self._empty_semaphore = Semaphore(self._size)
+        # Semaphore to check if consumers can act
         self._full_semaphore = Semaphore(0)
+        logger.log(logging.DEBUG, 'empty_semaphore starting value: ' +
+                   str(self._empty_semaphore._value))
+        logger.log(logging.DEBUG, 'full_semaphore starting value: ' +
+                   str(self._full_semaphore._value))
 
     def add_item(self):
+        logger.log(logging.DEBUG, 'Reached empty_semaphore check (acquire).')
         self._empty_semaphore.acquire()
+        logger.log(logging.DEBUG, 'empty_semaphore: ' +
+                   str(self._empty_semaphore._value))
 
         self._used += 1
 
+        logger.log(logging.DEBUG, 'Signaling full_semaphore (release).')
         self._full_semaphore.release()
+        logger.log(logging.DEBUG, 'full_semaphore: ' +
+                   str(self._full_semaphore._value))
 
     def remove_item(self):
+        logger.log(logging.DEBUG, 'Reached full_semaphore check (acquire).')
         self._full_semaphore.acquire()
+        logger.log(logging.DEBUG, 'full_semaphore: ' +
+                   str(self._full_semaphore._value))
 
         self._used -= 1
 
+        logger.log(logging.DEBUG, 'Signaling empty_semaphore (release).')
         self._empty_semaphore.release()
+        logger.log(logging.DEBUG, 'empty_semaphore: ' +
+                   str(self._empty_semaphore._value))
 
     def current_size(self):
+        # Probably shouldn't have a sanity check here, but it works!
         if self._used < 0 or self._used > self._size:
             exit(-1)
         return str(self._used).zfill(4)
@@ -50,8 +76,11 @@ class ProducerThread(Thread):
 
         super(ProducerThread, self).__init__()
         self.name = 'P' + str(number).zfill(4)
+        logger.log(logging.DEBUG, 'Initialized ' + self.name + ' with time: ' +
+                   str(self._production_time))
 
     def produce(self):
+        logger.log(logging.DEBUG, 'Producing item.')
         self._buffer.add_item()
 
     def run(self):
@@ -71,8 +100,11 @@ class ConsumerThread(Thread):
 
         super(ConsumerThread, self).__init__()
         self.name = 'C' + str(number).zfill(4)
+        logger.log(logging.DEBUG, 'Initialized ' + self.name + ' with time: ' +
+                   str(self._consumption_time))
 
     def consume(self):
+        logger.log(logging.DEBUG, 'Consuming item.')
         self._buffer.remove_item()
 
     def run(self):
@@ -91,25 +123,27 @@ def init_logging():
     logger_handler = logging.StreamHandler(sys.stdout)
     logger_handler.setFormatter(logger_format)
     logger.addHandler(logger_handler)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(LOG_LEVEL)
 
 if __name__ == '__main__':
 
-    buffer = LimitedBuffer(BUFFER_SIZE)
-
     init_logging()
+
+    buffer = LimitedBuffer(BUFFER_SIZE)
 
     producers = []
     consumers = []
 
     for i in range(int(PRODUCER_COUNT)):
-        producer = ProducerThread(buffer, len(producers), random.randint(2, 10))
+        producer = ProducerThread(buffer, len(producers),
+                                  random.randint(PROD_MIN, PROD_MAX))
 
         producers.append(producer)
         producer.start()
 
     for i in range(int(CONSUMER_COUNT)):
-        consumer = ConsumerThread(buffer, len(consumers), random.randint(2, 10))
+        consumer = ConsumerThread(buffer, len(consumers),
+                                  random.randint(CONS_MIN, CONS_MAX))
 
         consumers.append(consumer)
         consumer.start()
